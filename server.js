cffc538ec386caabec6f05083782b5907617b2a1
hjpark625 <zero950@gmail.com>
2024-04-19 11:14:37 +0900

    refactor: dist 폴더 부재 시 프로세스 종료 처리

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const path = require('path')
+const fs = require('fs')
+const chalk = require('chalk')
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'dist')))
+const distPath = path.join(__dirname, 'dist')
+
+if (!fs.existsSync(distPath)) {
+  console.error(chalk.red(`The dist folder doesn't exist. Please run ${chalk.blue('pnpm run build')} first.`))
+  process.exit(1)
+}
+
+app.use(express.static(distPath))
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
