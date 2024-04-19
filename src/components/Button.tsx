8c18c93c23648cbacf49323b93c5df7f1d8b9f4f
hjpark625 <zero950@gmail.com>
2024-04-19 10:15:31 +0900

    feat: consumer 완성

---
+++ src/components/Button.tsx
@@ -0,0 +1,16 @@
+function Button({ text }: { text?: string }) {
+  const handleClick = () => {
+    console.log('Button clicked')
+  }
+  return (
+    <button
+      className="w-[100px] h-[100px] border rounded-sm border-black bg-slate-400"
+      type="button"
+      onClick={handleClick}
+    >
+      {text ?? 'click me'}
+    </button>
+  )
+}
+
+export default Button
